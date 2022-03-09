59ec3490e8ca309d24282b27b27b294eaa284d7c
remant <remantmahato9798@gmail.com>
2022-03-10 03:58:01 +0530

    Added: main

---
+++ Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Snake_Ladder_Problem;
+
+class program
+{
+    static void Main(String[] args)
+    {
+        int position=0;
+        int Number_of_player = 1;
+        Console.WriteLine("Welcome to Snake and Ladder Game ");
+        Console.WriteLine("starting postion is : " + position);
+        Console.WriteLine("Number of Player : " + Number_of_player);
+    }
+}
+
+
+

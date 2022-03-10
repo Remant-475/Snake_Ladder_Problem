7ce1623e8f9f731dfdec3fa35abfb6bd3342adbb
remant <remantmahato9798@gmail.com>
2022-03-10 07:14:05 +0530

    Added: UC2-RollingDice

--- Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
+++ Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
@@ -6,12 +6,17 @@ class program
 {
     static void Main(String[] args)
     {
+        Random random= new Random();
+        int Diceroll=random.Next(1,7);
+
         int position=0;
         int Number_of_player = 1;
         Console.WriteLine("Welcome to Snake and Ladder Game ");
         Console.WriteLine("starting postion is : " + position);
         Console.WriteLine("Number of Player : " + Number_of_player);
+        Console.WriteLine("Dice roll : " + Diceroll);
     }
+
 }
 
 
